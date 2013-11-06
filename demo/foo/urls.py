2b87670cf3f44d0f2ef4cfcdff00ffe6a361fe51
Pablo Ricco <pricco@gmail.com>
2013-11-06 02:59:29 -0200

    applied pep8

--- demo/foo/urls.py
+++ demo/foo/urls.py
@@ -3,4 +3,6 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = patterns('',
-   url(r'^$', TemplateView.as_view(template_name='index.html')))
+                       url(r'^$',
+                           TemplateView.as_view(template_name='index.html'))
+                       )
