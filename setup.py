016e9610b703c9ed493155032b7877a2fec564d0
Pablo Ricco <pricco@gmail.com>
2016-09-29 17:24:35 -0300

    Bump version 0.0.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 
 setup(name='django-qurl-templatetag',
-      version='0.0.1',
+      version='0.0.2',
       packages=['qurltemplatetag', 'qurltemplatetag.templatetags'],
       license='MIT',
       author='Sophilabs',
